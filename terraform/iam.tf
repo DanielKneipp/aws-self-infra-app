43904bee2b52ffec51fabaddf08e792d45246c59
Daniel Kneipp <daniel.kneipp@outlook.com>
2022-10-01 13:52:57 +0200

    feat(terraform): only the GH role is need

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,21 +1,3 @@
-resource "aws_iam_role" "app_infra_manager_role" {
-  name = "app-infra-manager"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
 data "aws_iam_policy_document" "app_infra_manager_policy" {
   statement {
     sid    = "CreateAppRunnerServiceLinkeRole"
@@ -62,7 +44,7 @@ data "aws_iam_policy_document" "app_infra_manager_policy" {
   statement {
     sid       = "CloudformationAdmin"
     effect    = "Allow"
-    resources = ["arn:aws:cloudformation:*:*:*/*"]
+    resources = ["*"]
     actions   = ["cloudformation:*"]
   }
 }
@@ -70,22 +52,4 @@ data "aws_iam_policy_document" "app_infra_manager_policy" {
 resource "aws_iam_policy" "app_infra_manager_policy" {
   name   = "app_infra_manager_policy"
   policy = data.aws_iam_policy_document.app_infra_manager_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "app_infra_manager" {
-  role       = aws_iam_role.app_infra_manager_role.name
-  policy_arn = aws_iam_policy.app_infra_manager_policy.arn
-}
-
-resource "aws_iam_user" "app_infra_manager" {
-  name = "app_infra_manager"
-}
-
-resource "aws_iam_user_policy_attachment" "app_infra_manager" {
-  user       = aws_iam_user.app_infra_manager.name
-  policy_arn = aws_iam_policy.app_infra_manager_policy.arn
-}
-
-resource "aws_iam_access_key" "app_infra_manager" {
-  user = aws_iam_user.app_infra_manager.name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,3 @@
-output "app_infra_manager_secret_access_key" {
-  value     = aws_iam_access_key.app_infra_manager.secret
-  sensitive = true
-}
-
-output "app_infra_manager_access_key" {
-  value = aws_iam_access_key.app_infra_manager.id
-}
-
 output "gh_role_arn" {
   value = aws_iam_role.github.arn
 }
